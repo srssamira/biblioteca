03e811badd4d725fb5fd8ee209635abbdeaf37ed
Samira <srs.samira.silva@gmail.com>
2024-08-23 16:18:54 -0300

    add readme file

---
+++ readme.md
@@ -0,0 +1,1 @@
+<img src="static\umlBiblioteca.jpg" alt="uml Biblioteca">
--- src/IO.java
+++ src/IO.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class IO {
 
-
     public static Scanner input(String message){
         System.out.println(message);
         return new Scanner(System.in);
